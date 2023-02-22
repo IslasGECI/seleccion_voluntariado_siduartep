d9f89968b54923f655f7defbe38a46269abfcbe3
Simon Duarte <siimonduarte@gmail.com>
2023-02-22 15:01:26 -0800

    Adding black format

--- seleccion_voluntariado_2023/KATA_siduartep.py
+++ seleccion_voluntariado_2023/KATA_siduartep.py
@@ -1,5 +1,5 @@
-ERROR_DE_TIPO = 'EROR: El nombre del ave que introduciste no es de tipo string'
-ERROR_DE_STRING_VACIO = 'EROR: El nombre del ave no puede ser un str vacio'
+ERROR_DE_TIPO = "EROR: El nombre del ave que introduciste no es de tipo string"
+ERROR_DE_STRING_VACIO = "EROR: El nombre del ave no puede ser un str vacio"
 
 """"
     Este algoritmo recibe el nombre de un ave en formato cadena de texto (STRING) 
@@ -8,7 +8,7 @@ ERROR_DE_STRING_VACIO = 'EROR: El nombre del ave no puede ser un str vacio'
 """
 
 
-def generar_codigo(nombre_ave=''):
+def generar_codigo(nombre_ave=""):
     # Verificamos que el nombre del ave sea de tipo str, de caso contraario,
     # regresamos un error y el programa termina la ejecucion
     if not isinstance(nombre_ave, str):
@@ -18,14 +18,14 @@ def generar_codigo(nombre_ave=''):
     nombre_ave = nombre_ave.strip()
 
     # Verificamos que el nombre del ave no sea un str vacio
-    if nombre_ave == '':
+    if nombre_ave == "":
         raise ValueError(ERROR_DE_STRING_VACIO)
 
     # Establecemos que el input esté en mayúsculas, reemplazamos los guiones
     # con un guión y espacio porque es importante mantener el formato de guión
     # y del mismo modo es importante crear la separación para terminar así con
     # una separación de palabras
-    palabras = nombre_ave.upper().replace('-', '- ').split()
+    palabras = nombre_ave.upper().replace("-", "- ").split()
 
     # Regla 1: Si el nombre consta de una sola palabra, formamos el código de
     # las letras iniciales, hasta cuatro.
@@ -40,7 +40,12 @@ def generar_codigo(nombre_ave=''):
     # Regla 3: Para los nombres de tres palabras donde solo las dos últimas
     # palabras se unen con guión, el código usa las primeras dos letras de la
     # primera palabra y la primera letra de cada una de las dos últimas palabras.
-    elif len(palabras) == 3 and '-' in palabras[1] and not '-' in palabras[0] and not '-' in palabras[2]:
+    elif (
+        len(palabras) == 3
+        and "-" in palabras[1]
+        and not "-" in palabras[0]
+        and not "-" in palabras[2]
+    ):
         codigo = palabras[0][:2] + palabras[1][0] + palabras[2][0]
 
     # Regla 4: Para otros nombres con tres palabras, el código toma una letra de
