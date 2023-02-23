773aebf65a87f5a717892f9d680f0d2122db90f3
Simon Duarte <siimonduarte@gmail.com>
2023-02-22 21:44:03 -0800

    Update __init__.py

--- seleccion_voluntariado_2023/__init__.py
+++ seleccion_voluntariado_2023/__init__.py
@@ -1,3 +1,8 @@
+"""
+El algoritmo recibe el nombre de un ave y genera un código de cuatro letras bajo los siguientes criterios:
+[https://islas.dev/2020/08/06/kata]
+"""
+
 __version__ = "0.4.0"
 from .KATA_siduartep import generar_codigo, ERROR_DE_STRING_VACIO, ERROR_DE_TIPO
 
